ab86b8ebcf74fb5ce60945d0ee68e255cecc0c73
Cr1m <38981037+Cr1mson901@users.noreply.github.com>
2024-08-18 20:17:29 -0400

    Added notes

--- chapterOne/hello_world/main.rs
+++ chapterOne/hello_world/main.rs
@@ -3,3 +3,7 @@ fn main() {
     println!("Hello, World!");
 }
 //Running: rustc filePath creates an exe file that you can run//
+//Rust requires curly brackets around all function bodies
+//4 spaces instead of tab, unsure of the necessity
+//! means you are calling a macro instead of a normal function
+//Lines end in ; in rust
